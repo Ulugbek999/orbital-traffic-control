54e82f4fb8e9011c4e9a305b502b30a2d2a9c27c
Ulugbek999 <ulugbekabdurakhmonov.tennis@gmail.com>
2026-08-13 02:35:36 -0400

    Added a new satellite record

---
+++ app/lib/satellites.ts
@@ -0,0 +1,17 @@
+//A file where I will have all of the satellite TLEs.
+
+//Can use the following rout: https://celestrak.org/NORAD/elements/gp.php?CATNR=20580&FORMAT=TLE
+//substitue the numbers after CATNR= with the number of the satellite.
+
+
+
+//ISS (International space station)
+const ISS_TLE_LINE_1 = "1 25544U 98067A   26222.50435993  .00003786  00000+0  75820-4 0  9991";
+const ISS_TLE_LINE_2 = "2 25544  51.6324  31.2750 0007420  32.4605 327.6839 15.49403146580170";
+
+
+
+//Hubble
+const Hubble_TLE_LINE_1 = "1 20580U 90037B   26224.59154121  .00003205  00000+0  95532-4 0  9995";
+const Hubble_TLE_LINE_2 = "2 20580  28.4727  54.8959 0002463  53.8814 306.2010 15.31324458797226";
+
